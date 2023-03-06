4a22fb26c8069aac66bf99bd5536ae5abbab1c31
LAB-00\lab00-00 <e107ti@cps.sp.gov.br>
2023-03-06 20:32:08 -0300

    fuckpolice

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+Console.Write("Digite seu nome: ");
+string name = Console.ReadLine(); 
+Console.WriteLine("Olá " + (name) + "!");
+
+Console.Write("Digite seu ano de nascimento: ");
+int year = int.Parse (Console.ReadLine());
+int age = 2023 - year;
+Console.WriteLine("Você tem " + (age) + " anos");
+
+if (age >= 18)
+{
+    Console.WriteLine("Você é maior de idade");
+}
+
+else
+{
+    Console.WriteLine("Você é menor de idade");
+}
